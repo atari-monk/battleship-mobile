5f4b0161619baa814ca6c4dfad6735ddd0f8473b
atari-monk <atari.monk1@gmail.com>
2025-01-09 19:17:57 +0100

    feat(grid): make grid cover full screen

--- test/static_grid/script.js
+++ test/static_grid/script.js
@@ -1,8 +1,8 @@
 function generateGridItems() {
-  const container = document.querySelector('.grid-container')
+  const container = document.querySelector('.static-grid__grid')
   for (let i = 1; i <= 100; i++) {
     const gridItem = document.createElement('div')
-    gridItem.classList.add('grid-item')
+    gridItem.classList.add('static-grid__item')
     gridItem.textContent = i
     container.appendChild(gridItem)
   }
